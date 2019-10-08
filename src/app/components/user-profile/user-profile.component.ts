3afd4ea34140551365d0db533ee3bd801f5c979d
alexanderJPV <alexcyberay@gmail.com>
2019-10-08 14:21:42 -0400

    correccion formulario createuser

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,10 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  private user: any;
+  constructor(private userService: UserService, private authService: AuthService , private router: Router) { }
 
   ngOnInit() {
+    this.authService.getAccount().subscribe(
+      (res) => {
+        const data = res;
+        this.user=data;
+        console.log(this.user);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
